eee00603861a0e0e91c6328935af6d2f67a2f04d
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-02-22 18:37:41 +0300

    add DB interract functions

--- telegram_bot/management/commands/telegram-bot.py
+++ telegram_bot/management/commands/telegram-bot.py
@@ -1,6 +1,7 @@
 import logging
 from enum import Enum, auto
 
+import phonenumbers
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Updater,
@@ -13,6 +14,8 @@ from telegram.ext import (
 )
 from django.core.management.base import BaseCommand
 from django.conf import settings
+
+from telegram_bot.models import User
 
 logger = logging.getLogger(__name__)
 
@@ -64,3 +67,29 @@ def cancel(update: Update, context: CallbackContext) -> int:
     )
 
     return ConversationHandler.END
+
+
+def is_new_user(user_id):
+    return not User.objects.filter(user_id=user_id).exists()
+
+
+def save_user_data(data):
+    phone = f"{data['phone_number'].country_code}{data['phone_number'].national_number}"
+    User.objects.create(user_id=data["user_id"], full_name=data["full_name"], phonenumber=phone)
+
+
+def validate_fullname(fullname: list) -> bool | None:
+    if len(fullname) > 1:
+        return True
+
+
+def validate_phonenumber(number):
+    try:
+        parsed_number = phonenumbers.parse(number, 'RU')
+        return phonenumbers.is_valid_number_for_region(parsed_number, 'RU')
+    except phonenumbers.phonenumberutil.NumberParseException:
+        return False
+
+
+def delete_user(user_id):
+    User.objects.filter(user_id__contains=user_id).delete()
